d9b98cda4a81477c508dbd73a8c3a96947713dff
lowfnm <artem.melinevskiy21@gmail.com>
2022-01-25 17:57:57 +0200

    feat: balance module

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import GlobalStyles from "./style/global";
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme";
+import Balance from "../balance/Balance";
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <>
                 <GlobalStyles />
+                <Balance />
             </>
         </ThemeProvider>
     );
