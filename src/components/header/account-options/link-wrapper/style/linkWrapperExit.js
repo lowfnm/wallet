a76aae8d5b27365b69f5b2e4e752d4a242a78cb0
iamstorozhenko <iamantonstorozhenko@gmail.com>
2022-01-30 11:37:24 +0200

    feature: added modal logout

---
+++ src/components/header/account-options/link-wrapper/style/linkWrapperExit.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const LinkWrapper = styled.a`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const ExitLink = styled.span``;
+
+export { LinkWrapper, ExitLink };
