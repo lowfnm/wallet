13048f8b13217029f5aed0b6a363687a971b0e31
lowfnm <artem.melinevskiy21@gmail.com>
2022-02-01 14:07:22 +0200

    feat: diagram-tab

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,23 +1,26 @@
 import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import { chartData } from "./ChartData";
-import { ExpensesMoney, Section, Title, Wrapper } from "./style/chart";
+import { ExpensesMoney, Title, Wrapper } from "./style/chart";
 
 ChartJS.register(ArcElement, Tooltip);
 
 const Chart = () => {
     return (
-        <Section>
+        <div>
             <Title>Statistics</Title>
             <Wrapper>
                 <Doughnut
                     type="doughnut"
-                    options={{ cutout: 110, borderColor: "transparent" }}
+                    options={{
+                        cutout: 110,
+                        borderColor: "transparent",
+                    }}
                     data={chartData}
                 />
                 <ExpensesMoney>₴ 24 000.00</ExpensesMoney>
             </Wrapper>
-        </Section>
+        </div>
     );
 };
 
