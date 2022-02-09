3127c5751859214f243d3247113415c0098acf18
Melinevskiy Artem <artem.melinevskiy21@gmail.com>
2022-02-09 21:00:13 +0200

    Update HomeTabData.js

--- src/components/home-tab/HomeTabData.js
+++ src/components/home-tab/HomeTabData.js
@@ -5,70 +5,70 @@ const createData = (date, type, category, comments, amount, balance) => {
 const headers = ["date", "type", "category", "comments", "amount", "balance"];
 
 const transactions = [
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
-    // createData(
-    //     "04.01.19",
-    //     "-",
-    //     "Main expenses",
-    //     "A gift for wife",
-    //     300.0,
-    //     6900.0
-    // ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
+    createData(
+        "04.01.19",
+        "-",
+        "Main expenses",
+        "A gift for wife",
+        300.0,
+        6900.0
+    ),
 ];
 
 export { headers, transactions };
